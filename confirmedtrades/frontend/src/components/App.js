3a4c4e6f30246700f7feed3e9347e34642cd246c
integralfx <jordansu83@hotmail.com>
2020-01-13 15:22:49 +1100

    Add header

--- confirmedtrades/frontend/src/components/App.js
+++ confirmedtrades/frontend/src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import Header from "./layout/Header";
 
 class App extends Component {
   render() {
-    return <h2>Hello</h2>;
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <h2>Hello</h2>
+        </div>
+      </Fragment>
+    );
   }
 }
 
---
+++ confirmedtrades/frontend/src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Confirmed Trades
+          </a>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Users
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
